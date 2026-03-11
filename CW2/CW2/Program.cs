7bfbb24f15dc18d010730bdf8cd4be4c175026d3
Michał Brzóska <s32504@pjwstk.edu.pl>
2026-03-11 13:28:36 +0100

    Dodanie nowego brancha

--- CW2/CW2/Program.cs
+++ CW2/CW2/Program.cs
@@ -5,8 +5,22 @@ Console.WriteLine("cudem");
 Console.WriteLine("mnie jeszcze");
 Console.WriteLine("nie wyrzucili"); //yeah
 
+static double GetAverage(int[] numbers)
 
+{
+    double sum = 0;
+    if (numbers.Length == 0)
+    {
+        return 0;
+    }
 
+    for (int i = numbers.Length - 1; i >= 0; i--)
+    {
+        sum += numbers[i];
+
+    }
+    return sum / numbers.Length;
+}
 
 
 
